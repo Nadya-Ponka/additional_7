fe8c4af21e56f6aa506594d3a680108cfb277946
Nadya-Ponka <ponkratova@gmail.com>
2018-02-24 18:05:23 +0300

    Solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
+	
+  function solve(matrix) {
+  let i, j, was_find_decision, value;
+  
+  for (i = 0; i <= 8; i++) {
+    for (j = 0; j <= 8; j++) {
+
+	if (matrix[i][j] == 0) {
+        for (value = 1; value <= 9; value++) {
+          
+		  if (decision(matrix, i, j, value)) {
+            matrix[i][j] = value;
+            was_find_decision = solve(matrix);
+            if (was_find_decision) { return true; }
+            matrix[i][j] = 0;
+          }
+		  
+        }
+        return false;
+      }
+    }
+  }
+  return true;
+ }
+
+function decision(matrix, i, j, value) {
+  var a, b; // faster 
+  
+  for (a = 0; a <= 8; a++) {
+    if (a != i && matrix[a][j] == value) {
+      return false;
+    }
+  }
+  for (a = 0; a <= 8; a++) {
+    if (a != j && matrix[i][a] == value) {
+      return false;
+    }
+  }
+
+  var y = Math.floor((i / 3)) * 3,
+      x = Math.floor((j / 3)) * 3;
+  for (a = 0; a < 3; a++) {
+    for (b = 0; b < 3; b++) {
+      if (a != i && b != j && matrix[y + a][x + b] == value) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+solve(matrix);
+
+return matrix;
 }
